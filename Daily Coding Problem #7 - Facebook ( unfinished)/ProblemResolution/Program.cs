44d1f5feefd882489d657e7e93b626ff6ac88804
NicolasPCouts <34357034+NicolasPCouts@users.noreply.github.com>
2019-06-15 12:05:32 -0300

    first commit of the 7# problem
    its not solved yet

---
+++ Daily Coding Problem #7 - Facebook ( unfinished)/ProblemResolution/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemResolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWays = NumberOfWaysThatMessageCanBeDecoded("111");
+            Console.WriteLine(numberOfWays);
+            Console.ReadKey();
+        }
+
+        static int NumberOfWaysThatMessageCanBeDecoded(string message)
+        {
+
+        }
+    }
+}
